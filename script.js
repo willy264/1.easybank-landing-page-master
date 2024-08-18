eaf8c25056fa57da3232afedbfa47bfc29e8772e
U~Iriamuzu <daviswill264@gmail.com>
2024-08-18 21:01:35 +0100

    Project 1

---
+++ script.js
@@ -0,0 +1,69 @@
+// ACTIVE NAV-LINKS
+
+let navLink = document.querySelectorAll('.nav-link');
+  for (let i = 0; i < navLink.length; i++) {
+    let navValue = navLink[i]
+    navValue.addEventListener('click', () => {
+      navValue.classList.contains('active') ? navValue.classList.remove('active') : turnOffPrev(); navValue.classList.add('active');
+    })
+  }
+  
+function turnOffPrev() {
+  const prevBut = document.querySelector('.active');
+  if (prevBut) {
+    prevBut.classList.remove('active')
+  }
+}
+
+
+// MENU BUTTON
+const navMenu = document.getElementById('nav-menu'),
+          navToggle = document.getElementById('open-menu-button'),
+          navClose = document.getElementById('close-menu-button')
+
+
+if(navToggle){
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+    navToggle.style.display = 'none'
+    navClose.style.display = 'block'
+  })
+}
+
+if(navClose){
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+    navToggle.style.display = 'block'
+    navClose.style.display = 'none'
+  })
+}
+
+
+// REQUEST INVITE BUTTON
+const requestBtn = document.querySelectorAll('.invite-link')
+for (let j = 0; j < requestBtn.length; j++) {
+  let requestValue = requestBtn[j]
+  requestValue.addEventListener('click', () => {
+    if (!requestValue.classList.contains('activeLink')) {
+      requestValue.classList.add('activeLink')
+    } else (
+      requestValue.classList.remove('activeLink')
+    )
+  })
+}
+
+
+// FOOTER LINKS
+  let footerLink = document.querySelectorAll('.footer-link');
+  for (let i = 0; i < footerLink.length; i++) {
+    let footerValue = footerLink[i]
+    footerValue.addEventListener('click', () => {
+      footerValue.classList.contains('activeFooter') ? footerValue.classList.remove('activeFooter') : turnOffPrev(); footerValue.classList.add('activeFooter');
+    })
+  }
+  function turnOffPrev() {
+    const prevBut = document.querySelector('.activeFooter');
+    if (prevBut) {
+      prevBut.classList.remove('activeFooter')
+    }
+  }
